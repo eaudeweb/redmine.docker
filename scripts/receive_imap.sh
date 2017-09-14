bb27a8af72652e6b9c6d063586727e7b3d5832a7
Andrei Melis <andrei.melis@eaudeweb.ro>
2017-09-14 11:35:58 +0300

    Cron fixes

--- scripts/receive_imap.sh
+++ scripts/receive_imap.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+export PATH=/usr/local/bin:$PATH
+export GEM_HOME=/usr/local/bundle
+export GEM_PATH=/usr/local/bundle/gems:/usr/local/lib/ruby/gems/2.4.0
+export BUNDLE_APP_CONFIG=/usr/local/bundle
+export BUNDLE_BIN=/usr/local/bundle/bin
+export BUNDLE_PATH=/usr/local/bundle
+
 /usr/local/bundle/bin/rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
 	host=${RECEIVE_IMAP_HOST} port=${RECEIVE_IMAP_PORT} ssl=${RECEIVE_IMAP_SSL} \
 	username=${RECEIVE_IMAP_USERNAME} password=${RECEIVE_IMAP_PASSWORD} \
 	project=informea tracker=support status=new priority=normal \
-	allow_override=project,tracker,status,priority
+	allow_override=project,tracker,status,priority \
+	move_on_success=DONE
