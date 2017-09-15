0c6029654d62fdaa080d3a9adf9639d5f3f764dc
Andrei Melis <andrei.melis@eaudeweb.ro>
2017-09-15 18:16:11 +0300

    Default tracker and category for helpdesk inbound emails

--- scripts/receive_imap.sh
+++ scripts/receive_imap.sh
@@ -10,6 +10,6 @@ export BUNDLE_PATH=/usr/local/bundle
 /usr/local/bundle/bin/rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
 	host=${RECEIVE_IMAP_HOST} port=${RECEIVE_IMAP_PORT} ssl=${RECEIVE_IMAP_SSL} \
 	username=${RECEIVE_IMAP_USERNAME} password=${RECEIVE_IMAP_PASSWORD} \
-	project=informea tracker=support status=new priority=normal \
+	project=informea tracker=task status=new priority=normal category=helpdesk \
 	allow_override=project,tracker,status,priority \
 	move_on_success=DONE
