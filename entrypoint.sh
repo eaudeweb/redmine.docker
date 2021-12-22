d9f93aefeab7afb6eb0b8de056a67d63653e5913
root <root@artemis.edw.ro>
2021-12-22 11:27:26 +0200

    Redmine Theme & Plugins Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Generated environment for CRON jobs
-/usr/bin/printenv | sed 's/^\(.*\)$/export \1/g' | grep -E "^export TZ|RECEIVE_IMAP" > /var/local/redmine/scripts/env_receive_imap.sh
 /usr/bin/printenv | sed 's/^\(.*\)$/export \1/g' | grep -E "^export TZ|REDMINE_API_KEY" > /var/local/redmine/scripts/env_update_repositories.sh
 
 # Start cron in background
@@ -17,7 +16,10 @@ done
 /var/local/redmine/scripts/update_configuration.py
 
 gem install bundler --pre
+gem install activesupport
 
 bundle install
 
 /docker-entrypoint.sh rails server -b 0.0.0.0
+
+sleep 4800
