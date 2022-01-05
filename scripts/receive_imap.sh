f1cf6cd5858083b6f36053f62f418339330ccf66
Elif Mosessohn-Samedin <elif.samedin@gmail.com>
2022-01-05 18:38:02 +0200

    Removing environment variables causing Ruby related error (#11)

--- scripts/receive_imap.sh
+++ scripts/receive_imap.sh
@@ -2,10 +2,7 @@
 
 export PATH=/usr/local/bin:$PATH
 export GEM_HOME=/usr/local/bundle
-export GEM_PATH=/usr/local/bundle/gems:/usr/local/lib/ruby/gems/2.4.0
 export BUNDLE_APP_CONFIG=/usr/local/bundle
-export BUNDLE_BIN=/usr/local/bundle/bin
-export BUNDLE_PATH=/usr/local/bundle
 
 /usr/local/bundle/bin/rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
 	host=${RECEIVE_IMAP_HOST} port=${RECEIVE_IMAP_PORT} ssl=${RECEIVE_IMAP_SSL} \
