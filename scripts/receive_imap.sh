0b4093f2fa018589083cd77770ac4629c7e41407
Andrei Meliș <andrei.melis@eaudeweb.ro>
2022-06-10 15:08:44 +0300

    Update receive_imap.sh

--- scripts/receive_imap.sh
+++ scripts/receive_imap.sh
@@ -8,7 +8,7 @@ export BUNDLE_APP_CONFIG=/usr/local/bundle
 	host=${RECEIVE_IMAP_HOST} port=${RECEIVE_IMAP_PORT} ssl=${RECEIVE_IMAP_SSL} \
 	username=${RECEIVE_IMAP_USERNAME} password=${RECEIVE_IMAP_PASSWORD} \
 	project=${RECEIVE_IMAP_PROJECT} tracker=task status=new priority=normal category=helpdesk \
-	folder=${RECEIVE_IMAP_FOLDERS} project_from_subaddress=${RECEIVE_IMAP_USERNAME}
+	folder=${RECEIVE_IMAP_FOLDERS} project_from_subaddress=${RECEIVE_IMAP_USERNAME} \
 	allow_override=project,tracker,status,priority \
 	unknown_user=create no_permission_check=1 \
 	move_on_success=DONE move_on_failure=ERRORS
