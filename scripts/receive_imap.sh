d4ee1bd40a5f567869d4fc06e8415e78dab6800f
Andrei Meliș <andrei.melis@eaudeweb.ro>
2024-02-21 15:59:50 +0200

    Add missing param to receive imap script

--- scripts/receive_imap.sh
+++ scripts/receive_imap.sh
@@ -9,6 +9,7 @@ export BUNDLE_APP_CONFIG=/usr/local/bundle
 	username=${RECEIVE_IMAP_USERNAME} password=${RECEIVE_IMAP_PASSWORD} \
 	project=${RECEIVE_IMAP_PROJECT} tracker=task status=new priority=normal category=helpdesk \
 	folder=${RECEIVE_IMAP_FOLDERS} project_from_subaddress=${RECEIVE_IMAP_USERNAME} \
+	exclude_folders=${RECEIVE_IMAP_FOLDERS_EXCLUDE} \
 	allow_override=project,tracker,status,priority \
 	unknown_user=accept no_permission_check=1 \
 	move_on_success=DONE move_on_failure=ERRORS
